e78b6edc445b8aef681a0f50bc3f0b6ccc9700b0
Nick Aversano <nick23hi@gmail.com>
2019-05-24 19:25:05 -0400

    passing props to components

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ const coerceArray = (obj) =>
 const createElement = (type, props = null, children = null) => ({
   type,
   props,
-  children: coerceArray(children),
+  children: coerceArray(children)
 });
 
 // vnode: string | vnode
-const render = vnode => {  
+const render = vnode => {
+  console.log('in render', vnode);
   // Strings just convert to #text Nodes:
   if (typeof vnode === 'string') {
     return document.createTextNode(vnode);
@@ -20,37 +21,77 @@ const render = vnode => {
 
   // copy attributes onto the new node:
   const props = vnode.props || {};
-  Object.keys(props).forEach( k => n.setAttribute(k, props[k]) );
+  Object.keys(props).forEach(k => n.setAttribute(k, props[k]));
 
   // render (build) and then append child nodes:
-  (vnode.children || []).forEach(child => n.appendChild(render(child)) );
+  //(vnode.children || []).forEach(child => n.appendChild(render(child)));
+  
+  return n;
+}
 
+const renderVDOM = vnode => {
+  console.log('entering RENDER', vnode);
+  
+  //text node
+  if (typeof vnode === 'string') {
+    return render(vnode);
+  //html node
+  } else if (typeof vnode.type === 'string') {
+    vnode._render = () => render(vnode);
+  } else {
+  //react element
+    if (!vnode._inst) {
+      vnode._inst = new vnode.type(vnode.props);
+    }
+    
+    vnode._render = () => {
+      console.log('_render', vnode._inst.render());
+      //renderVDOM(vnode._inst.render());
+      return renderVDOM(vnode._inst.render());
+    }
+  }
+
+  const n = vnode._render();
+  vnode.children.forEach(child => n.appendChild(renderVDOM(child)));
+
+  console.log(n);
   return n;
 }
 
 const mount = (root, vnode) => {
-  function update() {
-    //if (root.childNodes[0]) root.removeChild(root.childNodes[0]);
-    //root.appendChild(renderTree(tree));
-
-    window.requestAnimationFrame(update);
-  }
-  update();
-
-  root.appendChild(render(vnode));
+  root.appendChild(renderVDOM(vnode));
 };
 
-class Component {}
-
-class Header extends Component {
+class Component {
+  constructor(props){
+    this.props = props || {};
+    this.state = {};
+  }
+  
+  setState = (state, callbackFn) => {
+    this.state = state;
+    callbackFn();
+  }
   render() {
-    return createElement('div', null, 'header');
+
   }
 }
 
+class Header extends Component {
+  render() {
+    return createElement('div', { class: "Header" }, this.props.name);
+  }
+}
+
+
+/*
 const tree = createElement('div', { style: "background: red;" }, [
-  createElement('div', null, 'hello world')
+  createElement(Header, null, 'hello world'),
+  createElement(Header, null, 'hello there'),
 ]);
+*/
+
+const tree = createElement(Header, {name: "hey"});
 
 console.log(tree);
 
